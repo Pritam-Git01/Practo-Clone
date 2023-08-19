affcd4191b4a0e65d66dfa335bd1a1b891a98068
Pritam-Git01 <pritamyadav.ds.work.@gmail.com>
2023-08-20 03:55:45 +0530

    changes booking Schema Pattern

--- Backend/Schema/Booking.js
+++ Backend/Schema/Booking.js
@@ -2,13 +2,13 @@ const mongooes = require("mongoose")
 
 
 const bookingDetails = new mongooes.Schema({
-    isPaid:{type:Boolean},
-    username:{type:String,required:true},
+    razorpayOrderId:{type:String,required:true, unique:true},   
+    patient_name:{type:String,required:true},
     phone:{type:String,required:true},
-    specialist:{type:String,required:true},
     amount:{type:Number,required:true},
-    razorpayPayment_id:{type:String,required:true},
-    razorpayOrderId:{type:String,required:true, unique:true}
+    date:{type:Object},
+    status:{type:String},
+    razorpayPaymentId:{type:String,required:true},
 })
 
 const bookingData = mongooes.model("bookingData", bookingDetails)
